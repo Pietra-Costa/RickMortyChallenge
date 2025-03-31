947b7a0f302669e9bedbb9a8ef0564319a3ff9cc
Pietra Costa <pietracosta.dev@gmail.com>
2025-03-31 18:59:04 -0300

    assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,23 @@ import { HomeComponent } from './pages/home/home.component';
 import { EpisodesComponent } from './pages/episodes/episodes.component';
 import { LocationComponent } from './pages/location/location.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/intro', pathMatch: 'full' },
   { path: 'intro', component: IntroComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'episodes', component: EpisodesComponent },
-  { path: 'location', component: LocationComponent },
-  { path: 'details/:id', component: DetailsComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'episodes', component: EpisodesComponent, canActivate: [AuthGuard] },
+  { path: 'location', component: LocationComponent, canActivate: [AuthGuard] },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
